6a1cb0ecb384e2edfa1dba33455368f5f23cd336
MelinaGomezTorres <mat.gomez@alumno.um.edu.ar>
2024-08-16 22:02:01 -0300

    Corrección de validación de coordenadas

--- chess/cli.py
+++ chess/cli.py
@@ -9,11 +9,15 @@ def play(chess):
     try:
         # print(chess.show_board())
         print("turn: ", chess.turn)
-        from_row = int(input("From row: "))
-        from_col = int(input("From col: "))
-        to_row = int(input("To Row: "))
-        to_col = int(input("To Col: "))
-        # :)
+        from_row = int(input("From row (0-7): "))
+        from_col = int(input("From col (0-7): "))
+        to_row = int(input("To Row (0-7): "))
+        to_col = int(input("To Col (0-7): "))
+
+#Aquí se verifica que las coordenadas estén en el rango 0-7, si no, lanza un error
+        if not (0 <= from_row <= 7 and 0 <= from_col <= 7 and 0 <= to_row <= 7 and 0 <= to_col <= 7):
+            raise ValueError("Las coordenadas deben estar entre 0 y 7.")
+
         chess.move(
             from_row,
             from_col,
@@ -21,7 +25,7 @@ def play(chess):
             to_col,
         )
     except Exception as e:
-        print("error", e)
+        print("Error", e)
 
 
 
