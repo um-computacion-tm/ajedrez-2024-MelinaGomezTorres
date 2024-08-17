923f40ba69e7bec1daef40a25eb7cc602435af40
MelinaGomezTorres <mat.gomez@alumno.um.edu.ar>
2024-08-17 20:54:38 -0300

    Actualizando test de chess

--- test/test_chess.py
+++ test/test_chess.py
@@ -1,7 +1,9 @@
-#Importo el módulo y la clase "chess" desde la carpeta con el mismo nombre
+#Importo el módulo y la clase "Chess" desde la carpeta con el mismo nombre
+#Importo más clases (Board y Piece)
 import unittest
 from chess.chess import Chess
-
+from chess.board import Board
+from chess.piece import Piece
 #Creo la clase correspondiente para este test 
 # Entro de setUp (se ejecuta automáticamente antes de cada prueba para asegurar que estas no se repitan)
 # Creo una nueva instancia del juego de ajedrez (self.game = Chess())
@@ -9,7 +11,11 @@ from chess.chess import Chess
 class TestChess(unittest.TestCase):
     
     def setUp(self):
-        self.game = Chess()
+        self.__game__ = Chess()
+
+#Verifica que el turno incial sea de las piezas blancas, casa contrario se mostrará un mensaje indicando el error
+    def test_initial_turn(self):
+        self.assertEqual(self.__game__.turn, "WHITE" , "El turno inicial debe ser de las piezas blancas")
 
 if __name__ == '__main__':
     unittest.main()
