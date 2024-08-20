e082a3c981b1b12bfcee598c14634830d7095741
MelinaGomezTorres <mat.gomez@alumno.um.edu.ar>
2024-08-20 19:51:45 -0300

    Archivos extendidos en clase

--- chess/cli.py
+++ chess/cli.py
@@ -1,22 +1,28 @@
-from chess import Chess
+from chess.chess import Chess
 
 def main():
     chess = Chess()
-    while True:
+    while chess.is_playing():
         play(chess)
 
 def play(chess):
     try:
-        # print(chess.show_board())
+        print(chess.show_board())
         print("turn: ", chess.turn)
-        from_row = int(input("From row (0-7): "))
-        from_col = int(input("From col (0-7): "))
-        to_row = int(input("To Row (0-7): "))
-        to_col = int(input("To Col (0-7): "))
+        from_row = int(input("From row: "))
+        from_col = int(input("From col: "))
+        to_row = int(input("To Row: "))
+        to_col = int(input("To Col: "))
+        
+#       from_row = int(input("From row (0-7): "))
+#       from_col = int(input("From col (0-7): "))
+#       to_row = int(input("To Row (0-7): "))
+#      to_col = int(input("To Col (0-7): "))
 
 #Aquí se verifica que las coordenadas estén en el rango 0-7, si no, lanza un error
-        if not (0 <= from_row <= 7 and 0 <= from_col <= 7 and 0 <= to_row <= 7 and 0 <= to_col <= 7):
-            raise ValueError("Las coordenadas deben estar entre 0 y 7.")
+#Esto va dentro del chess no del cli
+#       if not (0 <= from_row <= 7 and 0 <= from_col <= 7 and 0 <= to_row <= 7 and 0 <= to_col <= 7):
+#           raise ValueError("Las coordenadas deben estar entre 0 y 7.")
 
         chess.move(
             from_row,
@@ -24,6 +30,9 @@ def play(chess):
             to_row,
             to_col,
         )
+
+
+#Los except van desde el más particular al más general
     except Exception as e:
         print("Error", e)
 
--- chess/piezas/rook.py
+++ chess/piezas/rook.py
@@ -2,4 +2,9 @@ from chess.piece import Piece
 
 
 class Rook(Piece):
-    ...
+    
+    def __str__(self):
+        if self.__color__ == "WHITE":
+            return "♜"
+        else:
+            return "♖"
