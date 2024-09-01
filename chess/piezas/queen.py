8313211b6bc865bcea2c48135367188ddf2c1094
MelinaGomezTorres <mat.gomez@alumno.um.edu.ar>
2024-09-01 11:12:02 -0300

    Simbolos correctos para piezas

--- chess/piezas/queen.py
+++ chess/piezas/queen.py
@@ -1,6 +1,10 @@
 from chess.piece import Piece
 
-
+#Los símbolos de la reina se guardan como atributos
+#Luego, en el método __str__, selecciona el símbolo correcto basado en el color de la pieza 
 class Queen(Piece):
+    __white_symbol__ = "♛"
+    __black_symbol__ = "♕"
+
     def __str__(self):
-        return "♛" if self.__color__ == "WHITE" else "♕"
+        return self.__white_symbol__ if self.__color__ == "WHITE" else self.__black_symbol__
--- chess/piezas/rook.py
+++ chess/piezas/rook.py
@@ -1,7 +1,10 @@
 from chess.piece import Piece
 
-
+#Los símbolos de la torre se guardan como atributos
+#Luego, en el método __str__, selecciona el símbolo correcto basado en el color de la pieza 
 class Rook(Piece):
+    __white_symbol__ = "♜"
+    __black_symbol__ = "♖"
+
     def __str__(self):
-        return "♜" if self.__color__ == "WHITE" else "♖"
-#Este no va, va este 
+        return self.__white_symbol__ if self.__color__ == "WHITE" else self.__black_symbol__
