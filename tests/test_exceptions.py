c8e3735b0fb5471bed4f2fe8de3b25ebf5f1e3ef
MelinaGomezTorres <mat.gomez@alumno.um.edu.ar>
2024-09-27 10:44:27 -0300

    Función creada para evitar repeticiones a modo de test en exceptions

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,19 +1,24 @@
 import unittest
-from chess.exceptions import InvalidMove, InvalidTurn
+#from chess.exceptions import InvalidMove, InvalidTurn, EmptyPosition, OutOfBoard
 
 class TestChessExceptions(unittest.TestCase):
 
+    def check_exception(self, exception_class, expected_message):
+        with self.assertRaises(exception_class) as context:
+            raise exception_class()
+        self.assertEqual(str(context.exception), expected_message)
+
 #Verifica que cuando se lanza la excepción InvalidMove, el mensaje asociado es "Movimieto de pieza invalido"   
-    def test_invalid_move_exception(self):
-        with self.assertRaises(InvalidMove) as context:
-            raise InvalidMove()
-        self.assertEqual(str(context.exception), "Movimieto de pieza invalido")
+    #def test_invalid_move_exception(self):
+     #   with self.assertRaises(InvalidMove) as context:
+      #      raise InvalidMove()
+       # self.assertEqual(str(context.exception), "Movimieto de pieza invalido")
 
 #Prueba la excepción InvalidTurn y su mensaje   
-    def test_invalid_turn_exception(self):
-        with self.assertRaises(InvalidTurn) as context:
-            raise InvalidTurn()
-        self.assertEqual(str(context.exception), "No puedes mover pieza de otro jugador")
+    #def test_invalid_turn_exception(self):
+     #   with self.assertRaises(InvalidTurn) as context:
+      #      raise InvalidTurn()
+       # self.assertEqual(str(context.exception), "No puedes mover pieza de otro jugador")
     
 if __name__ == "__main__":
     unittest.main()
